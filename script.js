b0aac231a06dc830ed8afbaf7accb19b986170ae
Michael Banzeu <banzeumike@gmail.com>
2023-08-08 18:38:09 +0100

    Code updated (minor changes)

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ user_input.addEventListener('keydown', function (e) {
 });
 
 function addItem() {
-    var h2 = document.createElement("h2");
-    h2.innerHTML = "- " + user_input.value;
+    let h2 = document.createElement("h2");
 
+    h2.innerHTML = "- " + user_input.value;
     h2.addEventListener('click', function () {
         h2.style.textDecoration = "line-through";
     });
